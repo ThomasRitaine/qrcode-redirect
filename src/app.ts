ee50fe440c17e843a56247bd541d0ca25970904c
Thomas Ritaine <thomas.ritaine@amaris.com>
2023-10-12 12:12:56 +0200

    fix(Traefik): 🐛 Get the real IP forwarded by Traefik

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.get("/", (req: Request, res: Response) => {
 
     // Create a user connection asynchronously
     void createUserConnection(
-      (req.headers["X-Real-Ip"] as string) ?? "",
+      (req.get("X-Real-Ip") as string) ?? "",
       req.headers["user-agent"] ?? "",
       redirection?.id ?? "",
     );
